2dbdcfe1787acce6c498f724aca7b5b0a47843a4
Christopher Maree <christopher.maree@gmail.com>
2019-01-15 09:55:40 +0200

    Added uport-did to express endpoint

--- server.js
+++ server.js
@@ -2,19 +2,48 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var morgan = require('morgan');
+var didJWT = require('did-jwt')
 
-app.use(morgan('dev')); // log requests to the console
+app.use(morgan('dev'));
 
-// configure body parser
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080; // set our port
+var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
-router.get('/', function (req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+router.get('/validateJWT', function (req, res) {
+    if ('jwt' in req.body && 'did' in req.body) {
+        didJWT.verifyJWT(req.body.jwt, {
+            audience: req.body.did,
+            auth: true
+        }).then((response) => {
+            if (req.body.did == response.signer.owner) {
+                res.json({
+                    status: "SUCCESS",
+                    response: response
+                })
+            } else {
+                res.json({
+                    status:"ERROR",
+                    response:"Valid JWT but issuer DID does not match"
+                })
+            }
+        }).catch((error) => {
+            res.json({
+                status: "ERROR",
+                response: error.toString()
+            })
+        });
+
+    } else
+        res.json({
+            status: "ERROR",
+            response: "Incorrect request body! Must be formatted as {jwt:'',did:''}"
+        })
 });
 
 
